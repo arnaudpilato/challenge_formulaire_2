a657facd3758c9112086378cc1109bb974cc2c76
Pil <70975556+arnaudpilato@users.noreply.github.com>
2020-10-06 21:36:07 +0200

    Update form.php

--- form.php
+++ form.php
@@ -11,33 +11,33 @@
     <form  action="thanks.php"  method="post">
         <div>
             <label  for="lastname">Nom :</label>
-            <input  type="text"  id="lastname"  name="user_lastname">
+            <input  type="text"  id="lastname"  name="user_lastname" value="<?= $data['user_lastname'] ?? '' ?>" required>
         </div>
         <div>
             <label  for="firstname">Prénom :</label>
-            <input  type="text"  id="firstname"  name="user_firstname">
+            <input  type="text"  id="firstname"  name="user_firstname" value="<?= $data['user_firstname'] ?? '' ?>" required>
         </div>
         <div>
             <label  for="mail">Mail :</label>
-            <input  type="email"  id="mail"  name="user_email">
+            <input  type="email"  id="mail"  name="user_email" value="<?= $data['user_email'] ?? '' ?>" required>
         </div>
         <div>
             <label  for="phone">Phone :</label>
-            <input  type="tel"  id="phone"  name="user_phone">
+            <input  type="tel"  id="phone"  name="user_phone" value="<?= $data['user_phone'] ?? '' ?>" required>
         </div>
         <div>
             <label for="user_theme">Sujet :</label>
             <select name="user_theme" id="user_theme">
-                <option value="Tuyauterie">Tuyauterie</option>
-                <option value="Raton Laveur">Raton Laveur</option>
-                <option value="Bob l'éponge">Bob l'éponge</option>
-                <option value="Tartiflette">Tartiflette</option>
-                <option value="Manger des chips">Manger des chips</option>
+                <option value="La tuyauterie">Tuyauterie</option>
+                <option value="des ratons Laveurs">Raton Laveur</option>
+                <option value="de Bob l'éponge">Bob l'éponge</option>
+                <option value="d'une Tartiflette">Tartiflette</option>
+                <option value="de manger des chips">Manger des chips</option>
             </select>
         </div>
         <div>
             <label  for="message">Message :</label>
-            <textarea id="message"  name="user_message"></textarea>
+            <textarea id="message"  name="user_message" required><?= $data['user_message'] ?? '' ?></textarea>
         </div>
         <div  class="button">
             <button  type="submit">Envoyer votre message</button>
@@ -45,5 +45,3 @@
     </form>
 </body>
 </html>
-
-<?php
